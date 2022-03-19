d649c3172a789bebaaa906ce75dbcea7eea44dc4
Aleksander Jaworski <aleck156@gmail.com>
2022-03-19 13:45:52 +0100

    declare calcAverageHumanAge and add docs

--- challenge2.js
+++ challenge2.js
@@ -2,3 +2,9 @@
 
 const data1 = [5, 2, 4, 1, 15, 8, 3];
 const data2 = [16, 6, 10, 5, 6, 1, 4];
+
+/**
+ *
+ * @param {Array} ages an array of integers, dog's ages
+ */
+const calcAverageHumanAge = function (ages) {};
