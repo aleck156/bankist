22b3293b50394a11a3050a14d85426385740efe6
Aleksander Jaworski <aleck156@gmail.com>
2022-03-23 10:07:37 +0100

    learning how to manipulate DOM API using js

---
+++ lectures.js
@@ -0,0 +1,35 @@
+/*
+/////////////////////////////////////////////////
+// 185. How the DOM Really Works
+
+DOM - interface between browser and JS
+we can create/modify/delete HTML elements
+set styles, classes and attributes
+listen and respond to events
+
+
+Node - everything in DOM API is a node
+- has it's own properties, some are inherited
+- children
+    - Element - tons of useful properties!
+    - Text - stored within an elements
+    - Comment
+    - Document
+
+- Element
+    - HTMLElement
+      - 1 child type for each element type you can see on page, like link, p, img, form
+
+Inheritance
+- all the children have access to all of the fields and methods available to their parents
+- children also have their own, specific properties and methods
+
+.querySelector() method is available both on document and element objects
+
+EventTarget node
+the root of all nodes
+- contains methods like .addEventListener() and .removeEventListener()
+- all of his children inherit these two methods!
+
+we never manually create EventTarget node
+*/
--- script.js
+++ script.js
@@ -29,3 +29,83 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+///////////////////////////////////////
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+// .querySelector() grabs the first element that matches the criteria
+// returns a single HTMLElement
+const header = document.querySelector('.header');
+console.log(header);
+
+// .querySelectorAll() grabs all elements that meet the criteria
+// returns a NodeList, over which you can iterate
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+// .getElementById()
+// use only selector name, without . or #
+const s1 = document.getElementById('section--1');
+console.log(s1);
+
+// .getElementsByTagName()
+// returns HTMLCollection - an array of elements that are of type specified
+// it's ALIVE
+// every time DOM changes, this collection gets updated automatically
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+// .forEach()_ does not work on it, thus you have to loop it traditionally
+// allButtons.forEach(btn => console.log(btn));
+
+for (let i = 0; i < allButtons.length; i++) {
+  console.log(allButtons[i]);
+}
+
+// .getElementsByClassName()
+// also returns HTMLCollection - an array
+// it's ALIVE
+// every time DOM changes, this collection gets updated automatically
+const allBtns = document.getElementsByClassName('btn');
+console.log(allBtns);
+
+// creating and inserting elements
+// .insertAdjacentHTML()
+// very useful method!!
+// prepare string literals with a proper HTML code, inject data into it, then use it as argument to call this method
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+// message.textContent = `We use cookies for improved functionality and analytics
+// also, they tase so good ... :-)`;
+message.innerHTML = `We use cookies for improved functionality and analytics
+also, they taste so good ... :-) <button class="btn btn--close-cookie">Got it!</button>`;
+
+// this will attach only one message
+// message is alive, living inside a DOM
+header.prepend(message);
+
+// in order to attach the same object multiple times, you CLONE IT
+header.prepend(message.cloneNode(true));
+header.append(message.cloneNode(true));
+
+// appeding elements
+// .before() - attach before the element you're attaching it to
+// .after() - quite obvious, right?
+header.before(message.cloneNode(true));
+header.after(message.cloneNode(true));
+
+// before / prepend / append / after
+// document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+//   message.remove();
+// });
+
+// DOM traversing
+document.querySelectorAll('.btn--close-cookie').forEach(elem =>
+  elem.addEventListener('click', () => {
+    elem.parentElement.remove();
+  })
+);
